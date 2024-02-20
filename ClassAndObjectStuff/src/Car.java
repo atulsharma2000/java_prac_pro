f724b120f9e7373f9d6333f30b2d20e330890d04
atulsharma2000 <atullsharma2000@gmail.com>
2024-02-20 12:20:55 +0530

    ..

--- ClassAndObjectStuff/src/Car.java
+++ ClassAndObjectStuff/src/Car.java
@@ -5,20 +5,28 @@ public class Car {
    float curretnFuelInLiters;
    int noOfSeats;
 
-   public void drive(){
-
-        if(curretnFuelInLiters<=0)
-            System.err.println("Car out of fuel");
-        else if(curretnFuelInLiters<5)
-            System.err.println("Car in reserved mode, Refuel now !!");
-        else{
-            System.out.println("......Car is Driving........");
-            curretnFuelInLiters--;
-        }
+   public Car start(){
+    if(curretnFuelInLiters<=0)
+        System.err.println("Car out of fuel, can't start");
+    else if(curretnFuelInLiters<5){
+        System.err.println("Car Started in reserved mode, Refuel now !!");     
+    }
+    else{
+        System.out.println("Car is started........");
+    }
+    return this;   //returning current objects adderess of class Car
    }
 
-   public void addFuel(float fuel){
-        curretnFuelInLiters +=fuel;
+   public void drive(){
+        System.out.println("......Car is Driving........");
+        curretnFuelInLiters--; 
+   }
+
+   public void addFuel(float curretnFuelInLiters){
+        this.curretnFuelInLiters += curretnFuelInLiters;
+        // we could have given different name to local valriable but for understanding
+        // intially this.curretnFuelInLiters = 0 ,  then we add float argument curretnFuelInLiters here.
+        // using "this", we are making sure that current objects curretnFuelInLiters gets the added value from local variable curretnFuelInLiters
    }
    
    public float getCurrentFuelLevel(){
