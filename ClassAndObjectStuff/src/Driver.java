7ecf6c4c98163b4cab78881fbeb3927b41ea4b14
atulsharma2000 <atullsharma2000@gmail.com>
2024-02-20 12:21:18 +0530

    ...........

--- ClassAndObjectStuff/src/Driver.java
+++ ClassAndObjectStuff/src/Driver.java
@@ -2,10 +2,18 @@ public class Driver {
 
     public static void main(String[] args) {
         // interaction of Car class and driver class
-        Car mycar = new Car();
-        mycar.addFuel(6);
-        mycar.drive();
-        mycar.getCurrentFuelLevel();
+        // Car mycar = new Car();
+        // mycar.addFuel(6);
+        // mycar.drive();
+        // mycar.getCurrentFuelLevel();
+
+        Car xuvCar = new Car();   // object created and address stored in xuvCar(refrence vairable)
+        xuvCar.addFuel(6);
+        Car startedCar = xuvCar.start(); // statedCar holds same address as xuvCar
+        startedCar.drive();
+
+        // this keyword also helped here to directly call next method in same line [start() is returing "this"]
+        xuvCar.start().drive();
 
     }
    
