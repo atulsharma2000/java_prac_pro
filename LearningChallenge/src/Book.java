e890dc3f642a98ba9805872e1e9bc0994ad03f38
atulsharma2000 <atullsharma2000@gmail.com>
2024-02-20 17:03:52 +0530

    try fail cuz design asked is noob

--- LearningChallenge/src/Book.java
+++ LearningChallenge/src/Book.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Book {
     String title;
     String author;
-    int isbn;
+    String isbn;
     static int totalBooks;
+
+    Book(String title,String author, String isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+    Book(){
+        this("\0","\0","\0");
+    }
 
     public void borrowBook(){
 
@@ -12,10 +23,34 @@ public class Book {
     }
 
     public static void getTotalBooks(){
-
+        totalBooks++;
     }
 
     public static void main(String[] args) {
+        System.out.println("\n======Welcome to Library System=====");
+        Scanner sc = new Scanner(System.in);
         
+        System.out.print("Number of Books you want to Enter in Library:- ");
+        int numberOfEnteries = sc.nextInt();
+
+        Book[] obj = new Book[numberOfEnteries];  //array of objects
+        String a,b,c;
+
+        for(int i=0;i<numberOfEnteries;i++){
+            
+            System.out.println("Enter Details of Book["+(i+1)+"]: ");
+
+            System.out.print("Title: ");
+            a = sc.nextLine();
+            // System.out.print("\nAuthor: ");
+            // b = sc.nextLine();
+            // System.out.print("Isbn: ");
+            // c = sc.nextLine();
+            b="sad";
+            c="asd";
+
+            obj[i]  = new Book(a, b, c);
+        
+        }
     }
 }
