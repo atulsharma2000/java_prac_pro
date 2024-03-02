c7130e6ffdb7919a505562f6843a18b7d8286b8f
atulsharma2000 <atullsharma2000@gmail.com>
2024-03-02 06:51:57 +0530

    test

---
+++ OOPsPractice/src/RandomTest.java
@@ -0,0 +1,11 @@
+public class RandomTest {
+   
+    public static void main(String[] args) {
+        int a=1,b=0;
+        System.out.println(!(a<4));
+        
+        System.out.println(Integer.MAX_VALUE);
+
+
+    }
+}
